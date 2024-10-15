ada678c61b8d9f6585765a38cc53c67a950dea03
Santiago Baez <thiagoarias14@gmail.com>
2024-10-15 00:44:57 +0000

    ADD: agregue los pasos

---
+++ RTA_SCRIPT_Examen_20241012/Punto_A.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Cree la estructura de directorios
+sudo mkdir -p /Examenes-UTN/{alumno1/parcial{1,2,3},alumno2/parcial{1,2,3},alumno3/parcial{1,2,3},profesores}
+
+
+echo "Estructura de directorios creada"
+
+
